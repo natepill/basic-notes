2cde7ce49d213000d9475381212f98eeb228ccbc
Nathan Pillai <natepill@gmail.com>
2018-10-22 16:45:49 -0700

    introduction with yargs module and module.exports

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 console.log('Starting app');
 
 const fs = require('fs');
-const os = require('os');
+// const os = require('os');
 const notes = require('./notes.js');
+const yargs = require('yargs')
 
-var res = notes.addNote();
-console.log(res)
+const argv = yargs.argv;
+// var res = notes.addNote();
+// console.log(res)
+// var added = notes.add(3,4);
+// console.log(added)
 
-var added = notes.add(3,4);
-console.log(added)
+// var command = process.argv[2]
+var command = argv._[0]
+
+console.log(`Command: ${process.argv}`)
+console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+if (command === 'add'){
+    notes.addNote(argv.title, argv.body)
+}else if (command === 'list'){
+    notes.getAll();
+}else if(command === 'remove'){
+    notes.removeNote(argv.title);
+}else if(command === 'read'){
+    notes.readNote(argv.title);
+}
+else{
+    console.log('Command not recognized');
+}
 
 
 // var user = os.userInfo()
--- notes.js
+++ notes.js
@@ -1,11 +1,35 @@
 console.log('Starting notes.js');
 
+var addNote = (title, body) => {
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return  'New Note';
+    console.log('Adding note', title, body)
+
 };
 
-module.exports.add = (a, b) => {
-    return a + b
+
+var getAll = () => {
+    console.log('getting all notes')
 };
+
+var removeNote = (title) => {
+    console.log('removing ', title)
+};
+
+var readNote = (title) => {
+    console.log('reading ', title )
+};
+
+
+
+// If you want to use any of the functions or variables above, you must export them using ES6 conventions
+module.exports = {
+    addNote: addNote,
+    // For ES6 we can leave out the : addNote if we want because ES6 allows us set object attributes and values that are exactly the same we can just have the name of the function/variable to be assigned as the value
+    getAll: getAll,
+
+    removeNote: removeNote,
+
+    readNote: readNote
+
+
+}
