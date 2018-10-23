33a18c2c7ec7cfb1ce21a04d0ddd9a6f19a51ad7
Nathan Pillai <natepill@gmail.com>
2018-10-22 21:11:24 -0700

    add multiple notes and prevents duplicates

--- notes.js
+++ notes.js
@@ -1,9 +1,32 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+
 var addNote = (title, body) => {
 
-    console.log('Adding note', title, body)
+    var notes =  [];
+    var note =
+    {
+        title: title,
+        body: body
+    };
 
+    try{
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+
+    }catch(err){}
+
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+        //This function is called back once for every item in the array
+
+    if (duplicateNotes.length === 0){
+
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 
